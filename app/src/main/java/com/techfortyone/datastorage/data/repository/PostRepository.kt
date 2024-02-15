4db72c900723be4f5dba80d0e708b61aa619134d
Saikrishna41 <saai.krrishna@gmail.com>
2024-02-15 15:30:38 +0530

    store and read from cacheDir

--- app/src/main/java/com/techfortyone/datastorage/data/repository/PostRepository.kt
+++ app/src/main/java/com/techfortyone/datastorage/data/repository/PostRepository.kt
@@ -1,6 +1,7 @@
 package com.techfortyone.datastorage.data.repository
 
 import android.content.Context
+import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.techfortyone.datastorage.data.model.PostsItemDto
@@ -18,9 +19,17 @@ class PostRepository @Inject constructor(
 
     suspend fun getPosts(): List<PostsItemDto>? {
         return withContext(Dispatchers.IO) {
-            val result = postsApi.getPosts()
-            storeDataInFile(result.body())
-            result.body()
+            val postFromCache = readFromCache()
+            if (postFromCache?.isNotEmpty() == true) {
+                 postFromCache
+            }
+            else {
+                Log.d("TAGSS", "read from web service: ")
+                val result = postsApi.getPosts()
+                storeDataInFile(result.body())
+                storeDataInCache(result.body())
+                result.body()
+            }
         }
     }
 
@@ -30,5 +39,32 @@ class PostRepository @Inject constructor(
         val listType =  object : TypeToken<List<PostsItemDto>>() {}.type
         val fileContents = gson.toJson(posts, listType)
         file.writeText(fileContents)
+    }
+
+    fun storeDataInCache(posts: List<PostsItemDto>?) {
+        val file = File(context.cacheDir, "posts.json")
+        val gson = Gson()
+        val listType =  object : TypeToken<List<PostsItemDto>>() {}.type
+        val fileContents = gson.toJson(posts, listType)
+        file.writeText(fileContents)
+    }
+
+    fun readFromCache() : List<PostsItemDto>? {
+        Log.d("TAGSS", "readFromCache: ")
+        val cacheFile = File(context.cacheDir, "posts.json")
+        val json = if (cacheFile.exists()) {
+            cacheFile.readText()
+        } else {
+            null
+        }
+        return if (json == null) {
+            emptyList()
+        }
+        else {
+            val gson = Gson()
+            val listType =  object : TypeToken<List<PostsItemDto>>() {}.type
+            val fileContents = gson.fromJson<List<PostsItemDto>>(json, listType)
+            fileContents
+        }
     }
 }
